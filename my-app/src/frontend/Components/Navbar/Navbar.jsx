7c20a2fa4465a45b7d148ba43d23b1eeced2c771
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-05-22 10:53:11 +0530

    Changed navbar icons

--- my-app/src/frontend/Components/Navbar/Navbar.jsx
+++ my-app/src/frontend/Components/Navbar/Navbar.jsx
@@ -7,6 +7,8 @@ import { NavLink } from "react-router-dom";
 //import{FavoriteRoundedIcon} from "@mui/icons-material/FavoriteRounded"
 import {ShoppingCartOutlinedIcon} from '@mui/icons-material/ShoppingCartOutlined';
 import {BsCart2} from "react-icons/bs"
+import {AiOutlineHeart} from "react-icons/ai"
+import {AiOutlineUser} from "react-icons/ai"
 
 export const Navbar = () => {
   return (
@@ -14,6 +16,8 @@ export const Navbar = () => {
       <div className="first-nav">
         <h1>Sneakers Club</h1>
       </div>
+
+      
 
       {/* <div className="nav-image">
         <img src="https://d3t3ll7elkac89.cloudfront.net/prod/user/72e070ab-f786-4c99-9be5-42553d3eb6e0/4e0f6ed1-9432-49ec-8483-62e3d47c1b56/images/image0_1024_1024_watermark.jpg?Expires=1684593904&Signature=Xq4isZgO-L5QmJC8QW9h4gzJXE4AiYCD8XuJTaHogdKkwF~TI4xcGHsyQF10rn7FjU55zMPlJ7qTCdGziLupuNA5tV8amJw8NEQbEKkk2K1A-lHfbPD0IEWlG5RD9RI98EUmH30m3HDr9H0BC3QHyPygmSBDVZl6sh3JukOuTCphihSl5x4SSd1sF7uzRYElcYNBtcqDydiIfxRDp5Ltl8FROFNdPGIC9yddFxv4E6VbPlq-MSuAQtqyr~Ai4xt~tmBwZ8liKZ~jsumYs-tDGdn4LRgRWqAm2jizbpZeX1SCtaWt2XoslVIzdWJfb8lkwxxNrCpxOvDRQnHPUwtcDw__&Key-Pair-Id=K3RDDB1TZ8BHT8"/>
@@ -25,12 +29,13 @@ export const Navbar = () => {
       
 
       <div className="second-nav">
-        <NavLink></NavLink>
+        {/* <NavLink></NavLink> */}
         <NavLink to="/">Home</NavLink>
+        
         <NavLink to="/cart"><BsCart2/></NavLink>
-        <NavLink to="/wishlist">wishlist </NavLink>
-        <NavLink to="/login" >Log In</NavLink>
-        <NavLink to="/signup">SignUp</NavLink>
+        <NavLink to="/wishlist"><AiOutlineHeart/> </NavLink>
+        <NavLink to="/login" ><AiOutlineUser/></NavLink>
+        {/* <NavLink to="/signup">SignUp</NavLink> */}
        
       </div>
     </div>
