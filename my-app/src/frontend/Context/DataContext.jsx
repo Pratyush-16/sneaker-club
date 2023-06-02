c86e1e442ce5206b095d1d924c9d78dfdae59174
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-06-02 11:37:07 +0530

    removed comments

--- my-app/src/frontend/Context/DataContext.jsx
+++ my-app/src/frontend/Context/DataContext.jsx
@@ -1,26 +1,15 @@
 import axios from "axios";
 import React, { createContext, useEffect, useReducer, useState } from "react";
-  import { initialState, reducer } from "../Reducer/DataReduceer";
+import { initialState, reducer } from "../Reducer/DataReduceer";
 
 export const DataContext = createContext();
 
 export const DataContextProvider = ({ children }) => {
   const [sneakers, setSneakers] = useState(null);
-  //const [cart, setCart] = useState([]);
-  const [wishlist, setWishlist] = useState([])
 
-  // state   = {
-  //   cart:[],
-  //   wishlist:[],
-  //   filters:{
-  //       sort:"",
-  //       rating:0,
-  //       categorySelected:[],
-  //       price:9000
-  //   },
-  // }
- 
-  const[state,dispatch] = useReducer(reducer,initialState)
+  const [wishlist, setWishlist] = useState([]);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getSneakers = async () => {
     try {
@@ -36,8 +25,11 @@ export const DataContextProvider = ({ children }) => {
     getSneakers();
   }, []);
 
-  
   return (
-    <DataContext.Provider value={{ state, dispatch, sneakers,  wishlist, setWishlist}}>{children}</DataContext.Provider>
+    <DataContext.Provider
+      value={{ state, dispatch, sneakers, wishlist, setWishlist }}
+    >
+      {children}
+    </DataContext.Provider>
   );
 };
